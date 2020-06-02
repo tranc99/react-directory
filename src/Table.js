d9b2bcbe02dc7f0cdaad3cb8e5c003ba747eca3b
General Tomahawk <nomadhideout@yandex.com>
2020-06-02 19:20:24 +0200

    use multiple components

--- src/Table.js
+++ src/Table.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react'
+import TableHeader from './TableHeader'
+import TableBody from './TableBody'
 
 class Table extends Component {
   render() {
     return (
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Nancy</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Jaime</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Denise</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
+        <TableHeader />
+        <TableBody />
       </table>
     )
   }
---
+++ src/TableBody.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Nancy</td>
+        <td>Janitor</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Bouncer</td>
+      </tr>
+      <tr>
+        <td>Jaime</td>
+        <td>Aspiring actress</td>
+      </tr>
+      <tr>
+        <td>Denise</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  )
+}
+
+export default TableBody
